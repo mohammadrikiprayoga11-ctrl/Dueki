d2608b9f41b122e61055269651f6c6b69072b6f3
mohammadrikiprayoga11-ctrl <mohammadrikiprayoga11@gmail.com>
2026-02-11 19:42:10 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'dompetku-v10-3';
+const assets = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
+
+// Tahap Install: Menyimpan file ke dalam cache HP
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Tahap Fetch: Mengambil data dari cache jika sedang offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Tahap Aktivasi: Menghapus cache lama jika ada update
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
